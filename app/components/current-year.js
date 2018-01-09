809be2310674c107b55ac2054f927d5247ccb15b
Josh Lee <joshswl@gmail.com>
2018-01-09 16:52:44 -0600

    fixed {{year}} by putting it in the right hbs file

--- app/components/current-year.js
+++ app/components/current-year.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Component.extend({
-    year: Ember.computed(function() {
-        return moment().format('YYYY');
-    }),
+  year: Ember.computed(function() {
+    return moment().format('YYYY');
+  }),
 });
