0719d5c54c8b630e80471fe0f45daa42d453b799
Josh Lee <joshswl@gmail.com>
2018-03-09 22:53:17 -0600

    custom-packaging redesign + clean up styling

--- app/components/fade-in-and-out--custom-packaging.js
+++ app/components/fade-in-and-out--custom-packaging.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  items: [
+    {
+      image: 'custom-packaging-1.jpg',
+      number: '#1',
+      title: 'Bags & Colors!',
+      desc: 'Black Dog Surplus, Inc. has the ability to manufacture a MilSpec Ration Bag in any color, with two color printing.',
+    },
+    {
+      image: 'custom-packaging-2.jpg',
+      number: '#2',
+      title: 'Low Barrier!',
+      desc: 'With an extremely LOW Minimum Order Quantity of ONLY 1000 units per imprint, why not have a ration kit that is unique to your organization?',
+    },
+    {
+      image: 'custom-packaging-3.jpg',
+      number: '#3',
+      title: 'Get In Touch',
+      desc: 'Make YOUR ration kits YOUR OWN thru our Custom Packaging Program. Contact us today and lets get Creative, Together!',
+    },
+  ]
 });
